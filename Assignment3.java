1d1f20523b19696f1918ab8c1a7c3daba1f0ec1c
Xavier Williams <xkw.1602@gmail.com>
2023-02-21 13:10:54 -0600

    Added multList method

--- Assignment3.java
+++ Assignment3.java
@@ -1,11 +1,20 @@
 class Assignment3{
     
-    // Adds all elements in an array
+    // Adds all ints in an array and returns sum
     public static int addList(int[] arr){
         int sum = 0;
         for(int i : arr){
             sum += i;
         }
         return sum;
+    }
+
+    // Multiplies all ints in an array and returns product
+    public static multList(int[] arr){
+        int res = 1;
+        for(int i : arr){
+            res *= i;
+        }
+        return res;
     }
 }
